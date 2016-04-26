66907100b9112bdecb7aa0431bf03e5b2d9e8e93
Gabor Urbanics <gabor.urbanics@meltwater.com>
2016-04-26 10:00:30 +0200

    added simple test

---
+++ src/test/java/org/craftconf/SimpleTest.java
@@ -0,0 +1,14 @@
+package org.craftconf;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void simpleTest() {
+        assertEquals(true, false);
+    }
+
+}
